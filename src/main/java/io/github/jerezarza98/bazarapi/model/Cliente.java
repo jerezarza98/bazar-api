3efc9de006733df1b5c7be24adaa7de8ee11dc41
Jere Zarza <jerezarza98@gmail.com>
2026-07-21 02:21:19 -0300

    feat: convertir Cliente, Venta y Producto en entidades JPA

--- src/main/java/io/github/jerezarza98/bazarapi/model/Cliente.java
+++ src/main/java/io/github/jerezarza98/bazarapi/model/Cliente.java
@@ -1,4 +1,23 @@
 package io.github.jerezarza98.bazarapi.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private String dni;
 }
--- src/main/java/io/github/jerezarza98/bazarapi/model/Venta.java
+++ src/main/java/io/github/jerezarza98/bazarapi/model/Venta.java
@@ -1,4 +1,28 @@
 package io.github.jerezarza98.bazarapi.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Venta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate fechaVenta;
+    private Double total;
+
+    @ManyToMany
+    private List<Producto> productos;
+
+    @ManyToOne
+    private Cliente cliente;
 }
