b9b879aeb83187d67613e824ab5f58cad885280a
Jere Zarza <jerezarza98@gmail.com>
2026-07-22 02:06:53 -0300

    feat: agregar constructor parametrizado a Cliente

--- src/main/java/io/github/jerezarza98/bazarapi/model/Cliente.java
+++ src/main/java/io/github/jerezarza98/bazarapi/model/Cliente.java
@@ -20,4 +20,10 @@ public class Cliente {
     private String nombre;
     private String apellido;
     private String dni;
+
+    public Cliente(String nombre, String apellido, String dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
 }
